049b70c104512cb4e2bd139779b5ada23722e731
RyanBaten <rbb.baten@gmail.com>
2017-12-08 16:27:57 -0700

    added semicolon to company

--- database-setup/create-test.sql
+++ database-setup/create-test.sql
@@ -13,9 +13,11 @@ CREATE TABLE Postings
 INSERT INTO Postings (postingID, companyID, salary, location, description)
  VALUES
 (1,1,100000,"Denver","Machine Learning job, work with various types of NN for computer vision applications.");
+
 INSERT INTO Postings (postingID, companyID, salary, location, description)
  VALUES
 (2,2,125000,"San Francisco","Use various Machine Learning techniques to do cool stuff.");
+
 INSERT INTO Postings (postingID, companyID, salary, location, description)
  VALUES
 (3,3,50000,"Washington","Sales manager.");
@@ -30,12 +32,15 @@ CREATE TABLE Jobseeker
 INSERT INTO Jobseeker (jobStatus, username, userID)
  VALUES
 ("UNEMPLOYED", "John Doe", 1);
+
 INSERT INTO Jobseeker (jobStatus, username, userID)
  VALUES
 ("EMPLOYED_AND_SEARCHING", "John Ode", 2);
+
 INSERT INTO Jobseeker (jobStatus, username, userID)
  VALUES
 ("UNEMPLOYED", "Elvis Presley", 3);
+
 INSERT INTO Jobseeker (jobStatus, username, userID)
  VALUES
 ("EMPLOYED", "Queen Elizabeth", 4);
@@ -44,14 +49,16 @@ CREATE TABLE Company
 (
  username text,
  companyID int
-)
+);
 
 INSERT INTO Company (username, companyID)
  VALUES
 ("Websales Incorporated", 1);
+
 INSERT INTO Company (username, companyID)
  VALUES
 ("FindMeAJob Development", 2);
+
 INSERT INTO Company (username, companyID)
  VALUES
 ("Tech Startup CO", 3);
