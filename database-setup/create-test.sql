fcb015431acdbce0e0d3691ebbab0875a1d93d15
RyanBaten <rbb.baten@gmail.com>
2017-12-08 15:51:17 -0700

    added non-matching cases to db

--- database-setup/create-test.sql
+++ database-setup/create-test.sql
@@ -15,7 +15,10 @@ INSERT INTO Postings (postingID, companyID, salary, location, description)
 (1,1,100000,"Denver","Machine Learning job, work with various types of NN for computer vision applications.");
 INSERT INTO Postings (postingID, companyID, salary, location, description)
  VALUES
-(1,1,125000,"San Francisco","Use various Machine Learning techniques to do cool stuff.");
+(2,2,125000,"San Francisco","Use various Machine Learning techniques to do cool stuff.");
+INSERT INTO Postings (postingID, companyID, salary, location, description)
+ VALUES
+(3,3,50000,"Washington","Sales manager.");
 
 CREATE TABLE Jobseeker
 (
@@ -29,4 +32,10 @@ INSERT INTO Jobseeker (jobStatus, username, userID)
 ("UNEMPLOYED", "John Doe", 1);
 INSERT INTO Jobseeker (jobStatus, username, userID)
  VALUES
-("EMPLOYED_AND_SEARCHING", "John Ode", 1)
+("EMPLOYED_AND_SEARCHING", "John Ode", 2)
+INSERT INTO Jobseeker (jobStatus, username, userID)
+ VALUES
+("UNEMPLOYED", "Elvis Presley", 3)
+INSERT INTO Jobseeker (jobStatus, username, userID)
+ VALUES
+("EMPLOYED", "Queen Elizabeth", 4)
